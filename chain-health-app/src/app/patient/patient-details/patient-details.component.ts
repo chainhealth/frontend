71c3468d1787a378b43e4ce7ddff48258539ab89
peter <petersamy592@gmail.com>
2024-03-16 18:20:44 +0200

    trying somthing with the search

--- chain-health-app/src/app/patient/patient-details/patient-details.component.ts
+++ chain-health-app/src/app/patient/patient-details/patient-details.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-patient-details',
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrl: './patient-details.component.scss'
 })
 export class PatientDetailsComponent {
+  @Input() balance: number = 0; // input decorator allows flow from parent component
+  @Input() prescriptions: any[] = []; // Assuming prescriptions is an array of objects
 
 }
--- chain-health-app/src/app/patient/patient.component.ts
+++ chain-health-app/src/app/patient/patient.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrl: './patient.component.scss'
 })
 export class PatientComponent {
+  patientBalance: number = 500; 
+  patientPrescriptions: any[] = [
+    { name: 'Prescription 1', state: 'Pending' },
+    { name: 'Prescription 2', state: 'Rejected' },
+    { name: 'Prescription 3', state: 'Purchased' }
+  ]; // Example prescriptions
 
 }
