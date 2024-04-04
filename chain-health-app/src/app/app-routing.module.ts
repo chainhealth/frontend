cb7899a304ccfbb4b38c769c701822b5a2c33cc3
peter <petersamy592@gmail.com>
2024-04-04 20:24:15 +0200

    fixed navbar not showing on some pages, added stuff in app-routing that gives an error currently

--- chain-health-app/src/app/app-routing.module.ts
+++ chain-health-app/src/app/app-routing.module.ts
@@ -2,12 +2,23 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InsuranceComponent } from './insurance/insurance.component';
 import { InsuranceClaimsComponent } from './insurance/insurance-claims/insurance-claims.component';
+import { PatientComponent } from './patient/patient.component';
+import { PharmacyComponent } from './pharmacy/pharmacy.component';
+import { DoctorComponent } from './doctor/doctor.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { AuthGuard } from './auth.guard';
+
+
 const routes: Routes = [
-  {
-    path: 'insurance-claims/:patientId',
-    component: InsuranceClaimsComponent
-  },
-  { path: '', component: InsuranceComponent },
+  { path: 'insurance-claims/:patientId', component: InsuranceClaimsComponent },
+  { path: '', component: HomeComponent, canActivate: [AuthGuard] },
+  { path: 'login', component: LoginComponent },
+  { path: 'patient', component: PatientComponent, canActivate: [AuthGuard] },
+  { path: 'insurance', component: InsuranceComponent, canActivate: [AuthGuard] },
+  { path: 'pharmacy', component: PharmacyComponent, canActivate: [AuthGuard] },
+  { path: 'hospital', component: DoctorComponent, canActivate: [AuthGuard] },
+  { path: '**', redirectTo: '' } // Redirect any other routes to home
 ];
 
 @NgModule({
